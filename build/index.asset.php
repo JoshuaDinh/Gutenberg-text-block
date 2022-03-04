18663df32820d175b98ea9279f8dc6f4812cf9e9
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-04 14:01:08 -0800

    updated with RichText.Context component to recieve attributes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e7cccf7ca4fd597ac0a73cde8346a9c2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'cc311d49d90563ad2ebb40a1413ce1d3');
--- src/save.js
+++ src/save.js
@@ -1,10 +1,11 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { __ } from "@wordpress/i18n";
+import { useBlockProps, RichText } from "@wordpress/block-editor";
 
-export default function save() {
-	return (
-		<p {...useBlockProps.save()}>
-			{__('Boilerplate – hello from the saved content!', 'boilerplate')}
-		</p>
-	);
+export default function save({ attributes }) {
+  const { text } = attributes;
+  return (
+    // RichText.Content enables component to display text without editing features
+    // Takes in data from edit.js RichText component
+    <RichText.Content {...useBlockProps.save()} tagName="h4" value={text} />
+  );
 }
