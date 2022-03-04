99e25c55d5fe2b2a661d61b94db407a88f4971ab
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-04 13:05:04 -0800

    updated initial settings / meta data plugin-available in Wordpress

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '2db610ff0e017906cc94ad100713b25b');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
-import Edit from './edit';
-import save from './save';
+import { registerBlockType } from "@wordpress/blocks";
+import "./style.scss";
+import Edit from "./edit";
+import save from "./save";
 
-registerBlockType('create-block/boilerplate', {
-	edit: Edit,
-	save,
+registerBlockType("blocks/text-box", {
+  edit: Edit,
+  save,
 });
